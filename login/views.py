1a94c0a84a5685b150459fb40101b16316a45a50
choebaam <tmdak781@gmail.com>
2022-01-24 17:17:37 +0900

    bungae

---
+++ login/views.py
@@ -0,0 +1,36 @@
+from .models import user
+from django.utils import timezone
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        user_pw = request.POST.get('user_pw')
+        nickname = request.POST.get('nickname')
+
+        m = user(
+            user_id=user_id, user_pw=user_pw, nickname=nickname)
+        m.date_joined = timezone.now()
+        m.save()
+        return HttpResponse(
+            '가입 완료<br>%s %s %s' % (user_id, user_pw, nickname))
+    else:
+        return render(request, 'login/signup.html')
+
+
+# Create your views here.
+def login(request):
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        user_pw = request.POST.get('user_pw')
+        try:
+            m = user.objects.get(user_id=user_id, user_pw=user_pw)
+        except user.DoesNotExist as e:
+            return HttpResponse('로그인 실패')
+        else:
+            request.session['user_id'] = m.user_id
+            request.session['nickname'] = m.nickname
+        return redirect('http://127.0.0.1:8000/')
+    else:
+        return render(request, 'login/login.html')
