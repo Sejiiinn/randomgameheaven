c1194d95dba44ed3cf0d609754640ec32b8ae260
choebaam <tmdak781@gmail.com>
2022-01-25 17:06:11 +0900

    0125 번개 게시판 DB연동

--- bungae/urls.py
+++ bungae/urls.py
@@ -3,4 +3,5 @@ from . import views
 app_name = 'bungae'
 urlpatterns = [
     path('', views.bungae_board, name = 'bungae'),
+    
 ]
--- bungae/views.py
+++ bungae/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from bungae.models import *
+from login.models import user
 import datetime
 
 # Create your views here.
@@ -23,11 +24,29 @@ import datetime
 
 #     return render(request, 'bungae/bungae_board.html')
 
-def bungae_board(request):
-    now = datetime.datetime.now()
-    nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+# def bungae_board(request):
 
-    board = BungaeBoard.objects.get(bungae_num = 3)
-    board.cre_date = nowDatetime
-    board.save()
+#     board = BungaeBoard.objects.last()
+
+#     return render(request, 'bungae/bungae_board.html', {'board': board})
+
+def bungae_board(request):
+
+    board = BungaeBoard.objects.all()
+
+    get_id = request.session.get('user_id')
+
+    if get_id:
+        if request.method == 'POST':
+            now = datetime.datetime.now()
+            nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+
+            input_text = str(request.POST.get('input_text'))
+            user_id = str(get_id)
+
+            board = BungaeBoard(User = user.objects.get(user_id = user_id),content = input_text, cre_date = nowDatetime)
+            board.save()
+            return render(request, 'bungae/bungae_board.html', {'board': board})
+
+
     return render(request, 'bungae/bungae_board.html', {'board': board})
