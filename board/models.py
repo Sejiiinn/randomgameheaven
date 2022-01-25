38b72eb81ebef9ed5706fe878dddd249c3e1473d
Sejiiinn <rlatpwlsdlek@naver.com>
2022-01-25 15:01:52 +0900

    modal

--- board/models.py
+++ board/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Main(models.Model):
     game_id = models.IntegerField(primary_key=True)
     game_title = models.CharField(max_length = 50)
-    game_content = models.CharField(max_length = 50)
+    game_content = models.CharField(max_length = 255)
     game_img = models.CharField(max_length = 255)
 
     class Meta():
