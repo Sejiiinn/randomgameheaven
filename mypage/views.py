f0941e39c666b7506b11147bf88fe610f5583cfa
mjin-kim <myeoungjin523@gmail.com>
2022-01-25 15:09:14 +0900

    add profile

--- mypage/views.py
+++ mypage/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
+
+from mypage.forms import ChangePasswordForm
 from .models import board, user
-# from .forms import PWChangeForm
-# from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
 
 def mypage_main(request):
     userid = request.session.get('user_id')
@@ -13,9 +15,19 @@ def mypage_main(request):
         return render(request, 'mypage/mypage.html')
     else:
         return redirect('login:login')
+
+def chage_pw(request):
+    if request.method == 'POST':
+        change_pw_form = ChangePasswordForm(request.user, request.POST)
+        if change_pw_form.is_valid():
+            user = change_pw_form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, "비밀번호가 변경되었습니다.")
+            return redirect('mypage:mypage_main')
+    else:
+        change_pw_form = ChangePasswordForm(request.user)
     
-# def chage_pw(request):
-#     return render(request, 'mypage/change_pw.html')
+    return render(request, )
     
 def my_board(request):
     board_list = board.objects.all()
