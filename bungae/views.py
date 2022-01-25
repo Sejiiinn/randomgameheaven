b87ddd8a41056169937679fe33abd2ff39b1ad24
choebaam <tmdak781@gmail.com>
2022-01-25 10:39:50 +0900

    댓글에 닉네임, 시간 표시

--- bungae/views.py
+++ bungae/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
+
 # Create your views here.
 
 
 def bungae_board(request):
-    return render(request, 'bungae/bungae_board.html')
-
-def bungae_board(request):
     if request.method == 'POST':
-        a = request.POST.get('a')
+        input_text = request.POST.get('input_text')
 
-        result = '%s' % (a)
-        return render(request, 'bungae/bungae_board.html', {'result': result})
+        result = '%s' % (input_text)
+        now = datetime.datetime.now()
+        nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+        return render(request,
+                      'bungae/bungae_board.html', 
+                      {'result': result, 'user_id':request.session["user_id"], 'nowDatetime': nowDatetime})
+
     return render(request, 'bungae/bungae_board.html')
