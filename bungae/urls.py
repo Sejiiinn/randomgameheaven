b77098f7423e71a694355cd7791c41693f6b36a2
choebaam <tmdak781@gmail.com>
2022-01-25 09:27:11 +0900

    aa

--- bungae/urls.py
+++ bungae/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 app_name = 'bungae'
+
 urlpatterns = [
     path('', views.bungae_board, name = 'bungae'),
 ]
--- bungae/views.py
+++ bungae/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+
 def bungae_board(request):
+    if request.method == 'POST':
+        a = request.POST.get('input_text')
+
+        result = '%s' % (a)
+        return render(request, 'bungae/bungae_board.html', {'result':result})
     return render(request, 'bungae/bungae_board.html')
