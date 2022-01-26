1542640e2da96bec6dd52c05f8e93fa96bbc6101
JSJSWON <cowdog10@naver.com>
2022-01-26 15:38:14 +0900

    ㄹㄹ

--- recommendation/models.py
+++ recommendation/models.py
@@ -3,9 +3,22 @@ from login.models import user
 
 # Create your models here.
 
+class User(models.Model):
+    user_id = models.CharField(primary_key=True, max_length=50, unique=True)
+    user_pw = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'user'
+        app_label = 'recommendation'
+        managed = False
+
+
+
+
 class POST(models.Model):
-    user_game_id = models.IntegerField(primary_key=True, max_length=11, unique=True)
-    user_id = models.ForeignKey(user, on_delete=models.SET_NULL, null=True)
+    user_game_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     user_game_title = models.CharField(max_length=50)
     user_game_content = models.TextField()
     user_game_img = models.ImageField()
