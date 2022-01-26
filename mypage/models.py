ee8c152a2718758a2dbec8f8c2646c26e94bc0bc
mjin-kim <myeoungjin523@gmail.com>
2022-01-26 09:42:26 +0900

    modify models

--- mypage/models.py
+++ mypage/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.forms import ImageField
 from wordcloud import ImageColorGenerator
 
-class user(models.Model):
+class User(models.Model):
     user_id = models.CharField(primary_key=True, max_length=50)
     user_pw = models.CharField(max_length=50)
     nickname = models.CharField(max_length=50, unique=True)
@@ -15,7 +15,7 @@ class user(models.Model):
     
 class board(models.Model):
     board_num = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(user, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.TextField(null = True)
     content = models.TextField(null = True)
     cre_date = models.DateTimeField(null = True)
@@ -29,7 +29,7 @@ class board(models.Model):
         
 class Usergame(models.Model):
     user_game_id = models.IntegerField(primary_key=True)
-    user_id = models.ForeignKey(user, on_delete=CASCADE, max_length=50)
+    user = models.ForeignKey(User, on_delete=CASCADE, max_length=50)
     user_game_title = models.CharField(max_length=50)
     user_game_content = models.TextField()
     user_game_img = ImageField()
