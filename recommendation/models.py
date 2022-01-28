d026ac1551b84551f320bf28207134e22b62ea1e
JSJSWON <cowdog10@naver.com>
2022-01-28 10:20:58 +0900

    수정사항

--- recommendation/models.py
+++ recommendation/models.py
@@ -20,11 +20,18 @@ class Post(models.Model):
     user_game_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     user_game_title = models.CharField(max_length=50)
-    user_game_content = models.TextField()
+    user_game_content = models.CharField(max_length=300)
     user_game_img = models.CharField(max_length=50)
-    date_time = models.DateTimeField(auto_now_add=True)
+    date_time = models.DateTimeField()
+    # imgfile = models.ImageField(null=True, upload_to="", blank=True)
 
     class Meta:
         db_table = 'user_game'
         app_label = 'recommendation'
         managed = False
+
+
+# class FileUpload(models.Model):
+#     # title = models.TextField(max_length=40, null=True)
+#     imgfile = models.ImageField(null=True, upload_to="", blank=True)
+#     # content = models.TextField()
